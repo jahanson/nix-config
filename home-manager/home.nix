6d58dd54eea1dce7b49301e6267f82c09554ac66
Joseph Hanson <joe@veri.dev>
2024-02-20 22:06:59 -0600

    Revert "Disable starship"
    This reverts commit cb3af15492d1babdf61c6484c70d1fcc191e97c3.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -137,6 +137,18 @@
     rtx # rtx package manager
   ];
 
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
   # alacritty - a cross-platform, GPU-accelerated terminal emulator
   programs.alacritty = {
     enable = true;
