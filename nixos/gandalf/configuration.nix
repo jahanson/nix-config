6c2ac5e4a0b82c9fbb1293487ad55f33e425e6ec
Joseph Hanson <joe@veri.dev>
2024-03-15 14:13:09 -0500

    Cleaning up config.

--- nixos/gandalf/configuration.nix
+++ nixos/gandalf/configuration.nix
@@ -15,7 +15,7 @@
   boot = {
     supportedFilesystems = [ "zfs" ];
     zfs.forceImportRoot = false;
-    kernelParams = [ "zfs.zfs_arc_max=107374182400" ];
+    kernelParams = [ "zfs.zfs_arc_max=107374182400" ]; # 100GB
     zfs.extraPools = [ "eru" ];
   };
 
@@ -37,16 +37,9 @@
     lazydocker
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Services
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  # OpenSSH daemon.
   services.openssh = {
     enable = true;
     # require public key authentication for better security
@@ -182,26 +175,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
 
