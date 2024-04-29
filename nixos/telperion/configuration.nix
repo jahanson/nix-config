7c544320de12e0aefb0ddb6cb287ffd4ea38387f
Joseph Hanson <joe@veri.dev>
2024-04-29 12:18:49 -0500

    Remove.

--- nixos/telperion/configuration.nix
+++ nixos/telperion/configuration.nix
@@ -24,10 +24,7 @@
     };
   };
 
-  environment.etc."bind/zones/jahanson.tech.zone" = {
-    
-  };
-
+  
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
