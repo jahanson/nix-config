d91aa7b475cfbc1a1f88d34e2f13e6ba1ae53273
Joseph Hanson <joe@veri.dev>
2024-04-30 07:58:25 -0500

    Correct data path.

--- nixos/telperion/configuration.nix
+++ nixos/telperion/configuration.nix
@@ -81,7 +81,7 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.matchbox}/bin/matchbox -address=0.0.0.0:8080 -data-path=/srv/matchbox/data -log-level=debug";
+      ExecStart = "${pkgs.matchbox}/bin/matchbox -address=0.0.0.0:8080 -data-path=/srv/matchbox -log-level=debug";
       Restart = "on-failure";
       User = "matchbox";
       Group = "matchbox";
